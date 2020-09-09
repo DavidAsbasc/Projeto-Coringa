27b87336e8a0d930597dfdd6783bb8d3ae7b3ede
David Asbasc <70477783+DavidAsbasc@users.noreply.github.com>
2020-09-09 20:05:17 -0300

    atualização

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>projeto longin</title>
+    <link rel="stylesheet" href="projeto-coringa/css/estilo.css">
+</head>
+<body>
+    <div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="POST" action="processa.php">
+        <input type="cpf">
+        <input type="senha">
+        <input type="sbmit" value="ACESSAR">
+        <a href="">Esqueceu a senha?<strong>Cadastrar nova senha!</strong>/a>
+    </form>
+</div>
+</body>
+</htmk>
